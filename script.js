09450846f8f93dd5a39b8355917c44a87e692faa
yasminelisa <yasmin.7555966@aluno.mg.gov.br>
2025-10-22 08:27:39 -0300

    Carregamento dos arquivos do site calculadora
    Carregamento dos arquivos do site calculadora (projeto feito nas aulas de FrontEnd)

---
+++ script.js
@@ -0,0 +1,21 @@
+const display = document.getElementById("display");
+
+function appendSymbol (symbol){
+    display.value += symbol;
+}
+
+function clearDisplay(){
+    display.value ="";
+}
+
+function deleteLast(){
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate(){
+    try{
+        display.value=eval(display.value);
+    }catch{
+        display.value="Erro";
+    }
+}
